1f68e10a78282d6fede4e1c818ec50580e365837
kunkun9811 <timochen77@gmail.com>
2021-06-24 22:06:00 -0700

    fixation + time + distance + midpoint + velocity data done

--- server/data_process.py
+++ server/data_process.py
@@ -1,6 +1,7 @@
 from statistics import mean
 from math import sqrt
 
+# TODO: To be deleted
 def addOne(num):
         print("Before, num = ", num)
         num += 1
@@ -13,14 +14,19 @@ def addOne(num):
 def getPointsDist(x1,x2,y1,y2):
         return sqrt((x1-x2)**2 + (y1-y2)**2)
 
+# Check whether current point is at most "t2" distance away from rest of the points
+# Input (list, double, double, int): list of points, p1_x coordinate, p1_y coordinate, threshold 2 (diameter)
+# Output (bool): True=valid, False=not valid
 def isPointValidInCluster(cluster, p1_x, p1_y, t2):
         for p2 in cluster:
                 p2_x = p2[0]
                 p2_y = p2[1]
-                print("comparing with => ({x},{y})".format(x=p2_x, y=p2_y))
+                # print("comparing with => ({x},{y})".format(x=p2_x, y=p2_y), end=", ")
 
-                dist = getPointsDist(p1_x,p2_x,p1_y,p2_y)
-                if dist > t2:
+                diff = getPointsDist(p1_x,p2_x,p1_y,p2_y)
+
+                # print("diff =", diff)
+                if diff > t2:
                         return False
         return True
 
@@ -28,24 +34,47 @@ def isPointValidInCluster(cluster, p1_x, p1_y, t2):
 # Input (list, int): one cluster and the threshold 2 or the diameter
 # Output (list): New cluster satisfying t2 constraint
 def removeOutOfBoundPoints(cluster, t2):
-        new_cluster = []
-        for p1 in cluster:
+        # print(cluster)
+        new_cluster = {
+                "points": [],
+                "times": []
+        }
+        for idx in range(len(cluster['points'])):
+                p1 = cluster['points'][idx]
+                p1_time = cluster['times'][idx]
                 p1_x = p1[0]
                 p1_y = p1[1]
-                print("**Current point => ({x},{y})".format(x=p1_x, y=p1_y), end=" ")
-                if isPointValidInCluster(cluster, p1_x, p1_y, t2) == True:
-                        print("[SAVED]")
-                        new_cluster.append(p1)
-                else:
-                        print("[REMOVED]")
+                # print("**Current point => ({x},{y})".format(x=p1_x, y=p1_y), end=" ")
+                if isPointValidInCluster(cluster['points'], p1_x, p1_y, t2) == True:
+                        # print("[SAVED]")
+                        new_cluster["points"].append(p1)
+                        new_cluster["times"].append(p1_time)
+                # else:
+                #         print("[REMOVED]")
                 
-                print()
+                # print()
         
         return new_cluster
 
+def produceFixations(cluster_t2):
+        fixations = {}
+        for key in cluster_t2:
+                # last index of the array of times
+                lastIdx = len(cluster_t2[key]['times'])-1
+                startTime = cluster_t2[key]['times'][0]
+                endTime = cluster_t2[key]['times'][lastIdx]
+                duration = endTime - startTime
+                fixations[key] = {
+                        'points': [p for p in cluster_t2[key]['points']],
+                        'startTime': startTime,
+                        'endTime': endTime,
+                        'duration': duration
+                }
+        return fixations
+
 # Input (JSON): array of points, point = {"data": {x,y}, "elapsedTime": time}
 # Output (JSON): array of clusters, cluster = {""}
-# NOTE: Curent behavior is that 
+# **NOTE: Curent behavior is that points out of the diameter (greater than t2) are just removed
 def classify_fixation(data, t1, t2):
         if len(data) == 0:
                 print("*******NO DATA*******")
@@ -59,6 +88,7 @@ def classify_fixation(data, t1, t2):
         cur_cluster_id = 0
         cur_cluster_x = []
         cur_cluster_y = []
+        cur_cluster_times = []
         cur_cluster_mean_x = 0
         cur_cluster_mean_y = 0
         clusters = {}
@@ -67,40 +97,105 @@ def classify_fixation(data, t1, t2):
         for i in range(len(data)):
                 cur_y = data[i]['data']['y']
                 cur_x = data[i]['data']['x']
-
-                print("processing =>({x},{y})".format(x=cur_x, y=cur_y))
-
+                cur_t = data[i]['elapsedTime']
+                
+                # Get the midpoint coord. in the current cluster
                 cur_cluster_mean_x = mean(cur_cluster_x) if len(cur_cluster_x) > 0 else cur_x
                 cur_cluster_mean_y = mean(cur_cluster_y) if len(cur_cluster_y) > 0 else cur_y
+
                 dist = getPointsDist(cur_x, cur_cluster_mean_x, cur_y, cur_cluster_mean_y)
 
-
-                print("dist = {dist}".format(dist=dist))
-
                 # if distance is greater than the mean, create new cluster and flush out cur_clusters
-                if dist > t1 or i == len(data)-1:
-                        clusters[cur_cluster_id] = [xy for xy in zip(cur_cluster_x, cur_cluster_y)]
+                if dist > t1:
+                        # Add points and their corresponding times into "clusters"
+                        clusters[cur_cluster_id] = {
+                                "points": [xy for xy in zip(cur_cluster_x, cur_cluster_y)],
+                                "times": [t for t in cur_cluster_times]
+                        }
+
+                        cur_cluster_id += 1
                         cur_cluster_x.clear()
                         cur_cluster_y.clear()
+                        cur_cluster_times.clear()
                         cur_cluster_mean_x = 0
                         cur_cluster_mean_y = 0
-                        cur_cluster_id += 1
                 
                 cur_cluster_x.append(cur_x)
                 cur_cluster_y.append(cur_y)
+                cur_cluster_times.append(cur_t)
+        
+        if len(cur_cluster_x) > 0:
+                newEntry = {
+                        "points": [xy for xy in zip(cur_cluster_x, cur_cluster_y)],
+                        "times": cur_cluster_times
+                }
+                clusters[cur_cluster_id] = newEntry
 
-        # TODO: Might need to preserve time data
-        print("*****clusters*****")
+        print("***************clusters***************")
         print(clusters)
+        print()
         
+        # TODO: Modify parameters
         # Finalize fixations - using threshold 2
-        fixations = {}
+        cluster_t2 = {}
         for i in range(len(clusters)):
-                fixations[i] = removeOutOfBoundPoints(clusters[i],t2)
+                cluster_t2[i] = removeOutOfBoundPoints(clusters[i],t2)
 
-        print("*****fixations*****")
-        print(fixations)
-                
+        print("***************cluster_t2***************")
+        print(cluster_t2)
+        print()
 
+        fixations = produceFixations(cluster_t2)
 
+        # print("***************fixations (fixation + time)***************")
+        # print(fixations)
+        # print()
+
+        # print("Cluster size - fixation size =", len(clusters[0]['points']) - len(fixations[0]['points']))
+
+        return fixations
+
+# Distance between current point and previous point + mean of each fixation points
+def measureDistance(fixations):
+        prevFixMid = None
+        for key in fixations:
+                # first fixation, no distance
+                if key == 0:
+                        fixations[key]['distance'] = 0
+                        # get and set midpoint
+                        cur_x_coords = [p[0] for p in fixations[key]['points']]
+                        cur_y_coords = [p[1] for p in fixations[key]['points']]
+                        mean_x = mean(cur_x_coords)
+                        mean_y = mean(cur_y_coords)
+                        fixations[key]['midpoint'] = (mean_x, mean_y)
+                        prevFixMid = fixations[key]['midpoint']
+                else:
+                        # get midpoint
+                        cur_x_coords = [p[0] for p in fixations[key]['points']]
+                        cur_y_coords = [p[1] for p in fixations[key]['points']]
+                        mean_x = mean(cur_x_coords)
+                        mean_y = mean(cur_y_coords)
+                        fixations[key]['midpoint'] = (mean_x, mean_y)
+                        # get distance
+                        fixations[key]['distance'] = getPointsDist(prevFixMid[0], mean_x, prevFixMid[1], mean_y)
+                        prevFixMid = fixations[key]['midpoint']
+        return fixations
+
+# TODO: Need distance first
+# Output ()
+def measureVelocities(fixations):
+        prevEndTime = None
+        for key in fixations:
+                # first fixation, no velocity
+                if key == 0:
+                        prevEndTime = fixations[key]['endTime']
+                        fixations[key]['velocity(px/ms)'] = 0
+                        continue
+                # Calculate velocity w.r.t. previous fixation 
+                # NOTE: fixations[key]['distance'] = distance between current fixation's midpoint and previous fixation's midpoint
+                fixations[key]['velocity(px/ms)'] = fixations[key]['distance'] / (fixations[key]['startTime'] - prevEndTime)
+                prevEndTime = fixations[key]['endTime']
         
+        # TODO: getting divided by 0 because my elapsed time all the same
+
+        return fixations
--- server/server.py
+++ server/server.py
@@ -3,8 +3,11 @@ import json
 from data_process import *
 
 app = Flask(__name__)
+# NOTE: For production
 # threshold_1 = 100
 # threshold_2 = 200
+
+# NOTE: For development testing
 threshold_1 = 1
 threshold_2 = 2
 
@@ -22,7 +25,28 @@ def process():
                 print("Received data...")
                 print("Processing data...")
                 json_data = json.loads(request.data)
-                classify_fixation(json_data, threshold_1, threshold_2)
+                
+                # classify fixations
+                fixations = classify_fixation(json_data, threshold_1, threshold_2)
+
+                print("***************fixations (fixation + time)***************")
+                print(fixations)
+                print()
+
+                # measure distance between consecutive fixations
+                fixations = measureDistance(fixations)
+
+                print("***************fixations (fixation + time + distance + midpoint)***************")
+                print(fixations)
+                print()
+
+                # TODO: measure velocities between consecutive fixations
+                fixations = measureVelocities(fixations)
+
+                print("***************fixations (fixation + time + distance + midpoint + velocity)***************")
+                print(fixations)
+                print()
+                
                 res = make_response("Data Processed!", 200)
                 res.headers['Access-Control-Allow-Origin'] = '*'
                 return res
