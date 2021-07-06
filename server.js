cb90f973368dc51a716160fbc3de6b181ef684a8
kunkun9811 <timochen77@gmail.com>
2021-07-06 16:58:51 -0700

    Modified server.js for production - Secured port number

--- server.js
+++ server.js
@@ -2,14 +2,20 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+require("dotenv").config();
 
-const PORT_NUMBER = process.env.NODE_ENV === "production" ? process.env.PORT_NUMBER : 3000;
+console.log(`process.env.NODE_ENV = ${process.env.NODE_ENV}`);
+console.log(`process.eng.PORT_NUMBER = ${process.env.PORT_NUMBER}`);
+
+const PORT_NUMBER = process.env.NODE_ENV.trim() === "production" ? parseInt(process.env.PORT_NUMBER) : 3000;
 
 // specify build folder as the production directory
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "./public_html/build")));
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "./public_html/build", "index.html"));
 });
+
+console.log(`Listening on port ${PORT_NUMBER}...`);
 
 app.listen(PORT_NUMBER);
