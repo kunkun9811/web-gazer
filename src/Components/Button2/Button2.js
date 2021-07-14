e085da4186f63fa0a62f5d8af29fbd5c3829137f
kunkun9811 <timochen77@gmail.com>
2021-07-14 13:14:16 -0700

    Added easy reading and hard reading toggle

--- src/Components/Button2/Button2.js
+++ src/Components/Button2/Button2.js
@@ -2,7 +2,8 @@ import React from "react";
 import "./Button2.css";
 
 const Button2 = ({ label, BtnId, onClickHandler, selectedBtn }) => {
-  const isSelected = selectedBtn === BtnId ? "btn-2-selected" : "btn-2-not-selected";
+  // the "4" & "3" condition is for easy reading and hard reading
+  const isSelected = selectedBtn === BtnId ? "btn-2-selected" : selectedBtn === "4" && BtnId === "3" ? "btn-2-selected" : "btn-2-not-selected";
 
   return (
     <button onClick={() => onClickHandler(BtnId)} className={`btn-2 ${isSelected}`} id={BtnId}>
